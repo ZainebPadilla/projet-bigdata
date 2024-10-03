ca2b934a6590a44f357db542db8b16c6176f9a6e
ZainebPadilla <zainebt16@gmail.com>
2024-10-03 22:56:39 +0200

    maj exos

---
+++ lib/00_journalists.rb
@@ -0,0 +1,97 @@
+
+ # 1  nommer la liste
+ 
+liste_handle = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+countage_handle = liste_handle.length
+
+puts "Dans l'array, il y a #{countage_handle} handle"
+puts "_____________________________________________"
+puts "_____________________________________________"
+
+
+# 2 Quel est le handle le plus court de cette liste ?
+
+#créer une méthode de tri des handle par taille
+def trier(liste_handle)
+  comparer_handle = liste_handle.sort_by {|handle| handle.length}
+  puts "liste par ordre croissant en termes de nombre de caractère : #{comparer_handle} "
+  puts "le plus petit handle étant : #{comparer_handle[0]} " #existe plus simple #{tableau.min_by(&:length)}
+end
+
+trier(liste_handle)
+
+puts "_____________________________________________"
+puts "_____________________________________________"
+
+
+# 3 Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+def selectionner_cinq(liste_handle)
+  extraire_cinq = liste_handle.select {|handle| handle.length == 6}
+  puts "La liste contenant les handles à 5 caractères sans compter le @ est la suivante : #{extraire_cinq}"
+
+end
+
+selectionner_cinq(liste_handle)
+
+puts "_____________________________________________"
+puts "_____________________________________________"
+  
+# 4 Trie la liste de handle par ordre alphabétique.
+
+def listetriee(liste_handle) #Utiliser "sort" permet de trier automatiqueemnt apr ordre alphabétique
+liste_triee = liste_handle.sort 
+puts "Voici la liste affichée par ordre alphabétique :#{liste_triee} "
+end
+listetriee(liste_handle)
+puts "_____________________________________________"
+puts "_____________________________________________"
+
+# 5 Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+def listepartaille(liste_handle)
+  listetriee_partaille = liste_handle.sort_by {|handle| handle.length} #lenght = longueur
+  puts "Voici la liste triée apr taille : #{listetriee_partaille}"
+
+end
+
+listepartaille(liste_handle)
+
+puts "_____________________________________________"
+puts "_____________________________________________"
+
+# 6 Quelle est la position dans l'array de la personne @epenser ?
+# la fonction suivante n'a pas fonctionné car je suis dans un array et non dans un hash : puts " Le handle @epenser se situe en #{liste_handle[ "@epenser",]} place"
+#position = liste_handle.index(@epenser) avec puts "le handle "@epenser" se trouve en #{position + 1} position dans la liste" n'a pas fonctionné
+if                                                    # il faut utiliser la condition if pour vérifier que l'élément existe bien dans la liste
+  position = liste_handle.index("@epenser") # index sert à parcourir et chercher les éléments de la liste
+
+  puts "le handle @epenser se trouve en position #{position} de la liste"
+
+else
+  puts "Il n'y a pas de @epener dans la liste"
+
+end
+
+
+
+puts "_____________________________________________"
+puts "_____________________________________________"
+
+# 7 Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+  letter_count = liste_handle.map {|handle| handle.length}  # utiliser "map" permet de génèrer un tableau des longueurs de chaque handle
+
+  count = Hash.new(0)  # cela permet d'initialiser un hash pour compter les occurrences des tailles de handles
+
+
+  letter_count.each {|length| count[length] += 1} # Remplir le hash créé avec les comptages des caractères dans chaque handle (il va remplacer apr exemple "@epenser" par "8")
+
+  count.each do |length, occurrences|  # Afficher la répartition des handles par taille
+
+  puts "Il y a #{occurrences} handle de #{length} caractères"
+
+end
+
+puts "_______________________________"
+puts "______________FIN_________________"
+puts "_______________________________"
